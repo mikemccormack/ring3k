ba6299ca483bf472a3501b9391608c8adf4a5a66
Mike McCormack <mikem@ring3k.org>
2008-10-30 00:13:17 +0000

    Make the mailslot test send data

--- tests/mailslot.c
+++ tests/mailslot.c
@@ -20,6 +20,7 @@
 
 #include <stdarg.h>
 #include "ntapi.h"
+#include "rtlapi.h"
 #include "log.h"
 
 void test_mailslot(void)
@@ -100,6 +101,103 @@ void test_mailslot(void)
 	ok(r == STATUS_SUCCESS, "return wrong %08lx\n", r);
 }
 
+WCHAR mailslot_name[] = L"\\??\\mailslot\\foo";
+
+void mailslot_client( PVOID param )
+{
+	IO_STATUS_BLOCK iosb;
+	OBJECT_ATTRIBUTES oa;
+	HANDLE client, event;
+	UNICODE_STRING us;
+	NTSTATUS r;
+	ULONG i;
+
+	us.Buffer = mailslot_name;
+	us.Length = sizeof mailslot_name - 2;
+
+	oa.Length = sizeof oa;
+	oa.RootDirectory = 0;
+	oa.ObjectName = &us;
+	oa.Attributes = OBJ_CASE_INSENSITIVE;
+	oa.SecurityDescriptor = 0;
+	oa.SecurityQualityOfService = 0;
+
+	r = NtOpenFile( &client, GENERIC_READ | GENERIC_WRITE, &oa, &iosb, FILE_SHARE_READ|FILE_SHARE_WRITE, 0 );
+	ok(r == STATUS_SUCCESS, "return wrong %08lx\n", r);
+
+	r = NtCreateEvent( &event, EVENT_ALL_ACCESS, NULL, NotificationEvent, 0 );
+	ok(r == STATUS_SUCCESS, "return wrong (%08lx)\n", r);
+
+	for (i=0; i<10; i++)
+	{
+		// write a number
+		r = NtWriteFile( client, event, 0, 0, &iosb, &i, sizeof i, 0, 0 );
+		if (r == STATUS_PENDING)
+			r = NtWaitForSingleObject( event, TRUE, 0 );
+		ok (r == STATUS_SUCCESS, "write %ld returned %08lx\n", i, r);
+	}
+
+	r = NtClose( event );
+	ok(r == STATUS_SUCCESS, "return wrong %08lx\n", r);
+
+	r = NtClose( client );
+	ok(r == STATUS_SUCCESS, "return wrong %08lx\n", r);
+
+	NtTerminateThread( NtCurrentThread(), 0 );
+}
+
+void test_mailslot_server( void )
+{
+	HANDLE mailslot, thread, event;
+	IO_STATUS_BLOCK iosb;
+	OBJECT_ATTRIBUTES oa;
+	UNICODE_STRING us;
+	CLIENT_ID id;
+	NTSTATUS r;
+	ULONG i;
+
+	us.Buffer = mailslot_name;
+	us.Length = sizeof mailslot_name - 2;
+
+	oa.Length = sizeof oa;
+	oa.RootDirectory = 0;
+	oa.ObjectName = &us;
+	oa.Attributes = OBJ_CASE_INSENSITIVE;
+	oa.SecurityDescriptor = 0;
+	oa.SecurityQualityOfService = 0;
+
+	r = NtCreateMailslotFile( &mailslot, GENERIC_READ, &oa, &iosb, FILE_SHARE_WRITE, 0, 0, 0);
+	ok(r == STATUS_SUCCESS, "return wrong %08lx\n", r);
+
+	r = RtlCreateUserThread( NtCurrentProcess(), NULL, FALSE,
+							 NULL, 0, 0, &mailslot_client, NULL, &thread, &id );
+	ok( r == STATUS_SUCCESS, "failed to create thread\n" );
+
+	r = NtCreateEvent( &event, EVENT_ALL_ACCESS, NULL, NotificationEvent, 0 );
+	ok(r == STATUS_SUCCESS, "return wrong (%08lx)\n", r);
+
+	for (i=0; i<10; i++)
+	{
+		ULONG val = 0;
+
+		r = NtReadFile( mailslot, event, 0, 0, &iosb, &val, sizeof val, 0, 0 );
+		if (r == STATUS_PENDING)
+			r = NtWaitForSingleObject( event, TRUE, 0 );
+		ok (r == STATUS_SUCCESS, "read %ld returned %08lx\n", i, r);
+
+		ok( val == i, "value wrong\n");
+	}
+
+	r = NtClose( event );
+	ok(r == STATUS_SUCCESS, "return wrong %08lx\n", r);
+
+	r = NtWaitForSingleObject( thread, TRUE, 0 );
+	ok( r == STATUS_SUCCESS, "return wrong %08lx\n", r);
+
+	r = NtClose( mailslot );
+	ok(r == STATUS_SUCCESS, "return wrong %08lx\n", r);
+}
+
 // creation of \??\MAILSLOT is usually done at startup by smss.exe
 // create it for tests
 void create_mailslot_link( void )
@@ -133,5 +231,6 @@ void NtProcessStartup( void )
 	log_init();
 	create_mailslot_link();
 	test_mailslot();
+	test_mailslot_server();
 	log_fini();
 }
