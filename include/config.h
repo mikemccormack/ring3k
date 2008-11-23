6fc8854bbddd878c81b8aea7a289eb69211e3fb5
mike <mikem@ring3k.org>
2008-11-23 17:31:50 +0000

    Add SDL defines

--- include/config.h
+++ include/config.h
@@ -22,6 +22,12 @@
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
+/* Define if you have the SDL library */
+#define HAVE_SDL 1
+
+/* Define to 1 if you have the <SDL/SDL.h> header file. */
+#define HAVE_SDL_SDL_H 1
+
 /* Define to 1 if you have the `sigqueue' function. */
 #define HAVE_SIGQUEUE 1
 
