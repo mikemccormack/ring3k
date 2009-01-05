2ec54461995748a8a4fef24c03faa83f6a903489
mike <mikem@ring3k.org>
2009-01-05 19:10:42 +0000

    Add stock objects

--- tests/ntwin32.h
+++ tests/ntwin32.h
@@ -1,3 +1,5 @@
+// many definitions borrowed from Wine's winuser.h and wingdi.h
+
 #ifndef __NTWIN32_H__
 #define __NTWIN32_H__
 
@@ -187,6 +189,29 @@ typedef struct tagTEXTMETRICW {
 #define ANSI_CHARSET	      (BYTE)0
 #define DEFAULT_CHARSET       (BYTE)1
 #define OEM_CHARSET	      (BYTE)255
+
+#define WHITE_BRUSH	    0
+#define LTGRAY_BRUSH	    1
+#define GRAY_BRUSH	    2
+#define DKGRAY_BRUSH	    3
+#define BLACK_BRUSH	    4
+#define NULL_BRUSH	    5
+#define HOLLOW_BRUSH	    5
+#define WHITE_PEN	    6
+#define BLACK_PEN	    7
+#define NULL_PEN	    8
+#define OEM_FIXED_FONT	    10
+#define ANSI_FIXED_FONT     11
+#define ANSI_VAR_FONT	    12
+#define SYSTEM_FONT	    13
+#define DEVICE_DEFAULT_FONT 14
+#define DEFAULT_PALETTE     15
+#define SYSTEM_FIXED_FONT   16
+#define DEFAULT_GUI_FONT    17
+#define DC_BRUSH            18
+#define DC_PEN              19
+
+#define STOCK_LAST          19
 
 NTSTATUS NTAPI NtGdiInit(void);
 BOOLEAN NTAPI NtGdiComputeXformCoefficients(HANDLE);
