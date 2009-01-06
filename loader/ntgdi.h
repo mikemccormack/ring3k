0de5610126b3f39f0cd9b0f52e0d3a82908314ff
mike <mikem@ring3k.org>
2009-01-06 21:13:34 +0000

    Tidy list of stock objects

--- loader/ntgdi.h
+++ loader/ntgdi.h
@@ -22,24 +22,27 @@
 #define __NTGDI_H__
 
 // stock objects
-#define WHITE_BRUSH		 0
+#define WHITE_BRUSH		0
 #define LTGRAY_BRUSH		1
-#define GRAY_BRUSH		  2
+#define GRAY_BRUSH		2
 #define DKGRAY_BRUSH		3
-#define BLACK_BRUSH		 4
-#define NULL_BRUSH		  5
+#define BLACK_BRUSH		4
+#define NULL_BRUSH		5
 #define HOLLOW_BRUSH		5
-#define WHITE_PEN		   6
-#define BLACK_PEN		   7
-#define NULL_PEN			8
-#define OEM_FIXED_FONT	  10
-#define ANSI_FIXED_FONT	 11
-#define ANSI_VAR_FONT	   12
-#define SYSTEM_FONT		 13
-#define DEVICE_DEFAULT_FONT 14
-#define DEFAULT_PALETTE	 15
-#define SYSTEM_FIXED_FONT   16
+#define WHITE_PEN		6
+#define BLACK_PEN		7
+#define NULL_PEN		8
+#define OEM_FIXED_FONT		10
+#define ANSI_FIXED_FONT		11
+#define ANSI_VAR_FONT		12
+#define SYSTEM_FONT		13
+#define DEVICE_DEFAULT_FONT	14
+#define DEFAULT_PALETTE		15
+#define SYSTEM_FIXED_FONT	16
 #define DEFAULT_GUI_FONT	17
+#define DC_BRUSH		18
+#define DC_PEN			19
+#define STOCK_LAST		19
 
 // from FengYuan's Windows Graphics Programming Section 3.2, page 144
 
