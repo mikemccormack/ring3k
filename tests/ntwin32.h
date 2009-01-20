3dd60994c9d78cca3b6c186a948bb03371f1b8de
Mike McCormack <mikem@ring3k.org>
2009-01-20 19:06:13 +0000

    Use mingw32's winuser.h and wingdi.h headers

--- tests/ntwin32.h
+++ tests/ntwin32.h
@@ -4,6 +4,9 @@
 #define __NTWIN32_H__
 
 #include "ntapi.h"
+#include <stdarg.h>
+#include <wingdi.h>
+#include <winuser.h>
 
 #ifndef WINXP
 
@@ -37,183 +40,6 @@
 #define GDI_OBJECT_EMF     0x21
 #define GDI_OBJECT_PEN     0x30
 #define GDI_OBJECT_EXTPEN  0x50
-
-#define LF_FACESIZE     32
-#define LF_FULLFACESIZE 64
-
-#define DEFAULT_PITCH       0x00
-#define FIXED_PITCH         0x01
-#define VARIABLE_PITCH      0x02
-#define MONO_FONT           0x08
-
-#define FF_DONTCARE         0x00
-#define FF_ROMAN            0x10
-#define FF_SWISS            0x20
-#define FF_MODERN           0x30
-#define FF_SCRIPT           0x40
-#define FF_DECORATIVE       0x50
-
-typedef struct tagLOGFONTW
-{
-    LONG   lfHeight;
-    LONG   lfWidth;
-    LONG   lfEscapement;
-    LONG   lfOrientation;
-    LONG   lfWeight;
-    BYTE   lfItalic;
-    BYTE   lfUnderline;
-    BYTE   lfStrikeOut;
-    BYTE   lfCharSet;
-    BYTE   lfOutPrecision;
-    BYTE   lfClipPrecision;
-    BYTE   lfQuality;
-    BYTE   lfPitchAndFamily;
-    WCHAR  lfFaceName[LF_FACESIZE];
-} LOGFONTW, *PLOGFONTW, *LPLOGFONTW;
-
-typedef struct
-{
-  LOGFONTW elfLogFont;
-  WCHAR      elfFullName[LF_FULLFACESIZE];
-  WCHAR      elfStyle[LF_FACESIZE];
-  WCHAR      elfScript[LF_FACESIZE];
-} ENUMLOGFONTEXW,*LPENUMLOGFONTEXW;
-
-typedef struct
-{
-    LONG      tmHeight;
-    LONG      tmAscent;
-    LONG      tmDescent;
-    LONG      tmInternalLeading;
-    LONG      tmExternalLeading;
-    LONG      tmAveCharWidth;
-    LONG      tmMaxCharWidth;
-    LONG      tmWeight;
-    LONG      tmOverhang;
-    LONG      tmDigitizedAspectX;
-    LONG      tmDigitizedAspectY;
-    WCHAR     tmFirstChar;
-    WCHAR     tmLastChar;
-    WCHAR     tmDefaultChar;
-    WCHAR     tmBreakChar;
-    BYTE      tmItalic;
-    BYTE      tmUnderlined;
-    BYTE      tmStruckOut;
-    BYTE      tmPitchAndFamily;
-    BYTE      tmCharSet;
-    DWORD     ntmFlags;
-    UINT      ntmSizeEM;
-    UINT      ntmCellHeight;
-    UINT      ntmAvgWidth;
-} NEWTEXTMETRICW, *PNEWTEXTMETRICW, *LPNEWTEXTMETRICW;
-
-typedef struct
-{
-  DWORD fsUsb[4];
-  DWORD fsCsb[2];
-} FONTSIGNATURE, *PFONTSIGNATURE, *LPFONTSIGNATURE;
-
-typedef struct
-{
-    NEWTEXTMETRICW	ntmTm;
-    FONTSIGNATURE       ntmFontSig;
-} NEWTEXTMETRICEXW;
-
-#if 0
-#define MM_MAX_AXES_NAMELEN 16
-#define MM_MAX_NUMAXES 16
-
-typedef struct tagAXISINFO
-{
-  LONG axMinValue;
-  LONG axMaxValue;
-  WCHAR axAxisName[MM_MAX_AXES_NAMELEN];
-} AXISINFOW, *PAXISINFOW;
-
-typedef struct tagAXESLIST {
-  DWORD    axlReserved;
-  DWORD    axlNumAxes;
-  AXISINFOW axlAxisInfo[MM_MAX_NUMAXES];
-} AXESLISTW, *PAXESLISTW;
-
-typedef struct tagENUMTEXTMETRIC
-{
-  NEWTEXTMETRICEXW etmNewTextMetricEx;
-  AXESLISTW etmAxesList;
-} ENUMTEXTMETRICW, *PENUMTEXTMETRICW;
-#endif
-
-typedef struct tagTEXTMETRICW {
-    LONG      tmHeight;
-    LONG      tmAscent;
-    LONG      tmDescent;
-    LONG      tmInternalLeading;
-    LONG      tmExternalLeading;
-    LONG      tmAveCharWidth;
-    LONG      tmMaxCharWidth;
-    LONG      tmWeight;
-    LONG      tmOverhang;
-    LONG      tmDigitizedAspectX;
-    LONG      tmDigitizedAspectY;
-    WCHAR     tmFirstChar;
-    WCHAR     tmLastChar;
-    WCHAR     tmDefaultChar;
-    WCHAR     tmBreakChar;
-    BYTE      tmItalic;
-    BYTE      tmUnderlined;
-    BYTE      tmStruckOut;
-    BYTE      tmPitchAndFamily;
-    BYTE      tmCharSet;
-} TEXTMETRICW, *LPTEXTMETRICW, *PTEXTMETRICW;
-
-#define RASTER_FONTTYPE     0x0001
-#define DEVICE_FONTTYPE     0x0002
-#define TRUETYPE_FONTTYPE   0x0004
-
-#define FW_DONTCARE	    0
-#define FW_THIN		    100
-#define FW_EXTRALIGHT	    200
-#define FW_ULTRALIGHT	    200
-#define FW_LIGHT	    300
-#define FW_NORMAL	    400
-#define FW_REGULAR	    400
-#define FW_MEDIUM	    500
-#define FW_SEMIBOLD	    600
-#define FW_DEMIBOLD	    600
-#define FW_BOLD		    700
-#define FW_EXTRABOLD	    800
-#define FW_ULTRABOLD	    800
-#define FW_HEAVY	    900
-#define FW_BLACK	    900
-
-#define ANSI_CHARSET	      (BYTE)0
-#define DEFAULT_CHARSET       (BYTE)1
-#define OEM_CHARSET	      (BYTE)255
-
-#define WHITE_BRUSH	    0
-#define LTGRAY_BRUSH	    1
-#define GRAY_BRUSH	    2
-#define DKGRAY_BRUSH	    3
-#define BLACK_BRUSH	    4
-#define NULL_BRUSH	    5
-#define HOLLOW_BRUSH	    5
-#define WHITE_PEN	    6
-#define BLACK_PEN	    7
-#define NULL_PEN	    8
-#define OEM_FIXED_FONT	    10
-#define ANSI_FIXED_FONT     11
-#define ANSI_VAR_FONT	    12
-#define SYSTEM_FONT	    13
-#define DEVICE_DEFAULT_FONT 14
-#define DEFAULT_PALETTE     15
-#define SYSTEM_FIXED_FONT   16
-#define DEFAULT_GUI_FONT    17
-#define DC_BRUSH            18
-#define DC_PEN              19
-
-#define STOCK_LAST          19
-
-#define RGB(r,g,b)          ((COLORREF)((BYTE)(r) | ((BYTE)(g) << 8) | ((BYTE)(b) << 16)))
 
 NTSTATUS NTAPI NtGdiInit(void);
 BOOLEAN NTAPI NtGdiComputeXformCoefficients(HANDLE);
@@ -272,14 +98,6 @@ typedef struct _NTSIMPLEMESSAGEPACKEDINFO {
 	ULONG	(CALLBACK *func)(PVOID,ULONG,WPARAM,LPARAM,PVOID);
 } NTSIMPLEMESSAGEPACKEDINFO;
 
-typedef struct tagMINMAXINFO {
-	POINT	ptReserved;
-	POINT	ptMaxSize;
-	POINT	ptMaxPosition;
-	POINT	ptMinTrackSize;
-	POINT	ptMaxTrackSize;
-} MINMAXINFO, *PMINMAXINFO, *LPMINMAXINFO;
-
 typedef struct _NTMINMAXPACKEDINFO {
 	PVOID*	wininfo;
 	ULONG	msg;
@@ -324,21 +142,6 @@ typedef struct _NTCREATEPACKEDINFO {
 	ULONG	(CALLBACK *func)(PVOID,ULONG,WPARAM,NTCREATESTRUCT,PVOID);
 } NTCREATEPACKEDINFO;
 
-typedef struct _WINDOWPOS {
-	HANDLE	hwnd;
-	HANDLE	hwndInsertAfter;
-	INT	x;
-	INT	y;
-	INT	cx;
-	INT	cy;
-	UINT	flags;
-} WINDOWPOS;
-
-typedef struct _NCCALCSIZE_PARAMS {
-	RECT	rgrc[3];
-	WINDOWPOS *lppos;
-} NCCALCSIZE_PARAMS;
-
 typedef struct _NTNCCALCSIZEPACKEDINFO {
 	PVOID*	wininfo;
 	ULONG	msg;
@@ -358,49 +161,6 @@ typedef struct _NTMOVINGPACKEDINFO {
 	ULONG	(CALLBACK *func)(PVOID,ULONG,WPARAM,VOID*,PVOID);
 } NTMOVINGPACKEDINFO;
 
-#define WM_CREATE           0x0001
-#define WM_MOVE             0x0003
-#define WM_ACTIVATE         0x0006
-#define WM_SHOWWINDOW       0x0018
-#define WM_NCCREATE         0x0081
-#define WM_NCCALCSIZE       0x0083
-#define WM_GETMINMAXINFO    0x0024
-#define WM_MOVING           0x0216
-
-#define CS_VREDRAW          0x00000001
-#define CS_HREDRAW          0x00000002
-#define CS_DBLCLKS          0x00000008
-#define CS_PARENTDC         0x00000080
-#define CS_GLOBALCLASS      0x00004000
-
-#define WS_CLIPSIBLINGS  0x04000000L
-#define WS_CAPTION       0x00C00000L
-#define WS_BORDER        0x00800000L
-#define WS_DLGFRAME      0x00400000L
-#define WS_SYSMENU       0x00080000L
-#define WS_GROUP         0x00020000L
-
-#define WS_EX_WINDOWEDGE       0x00000100L
-
-#define CW_USEDEFAULT ((INT)0x80000000)
-
-#define SW_HIDE             0
-#define SW_SHOWNORMAL       1
-#define SW_NORMAL           1
-#define SW_SHOWMINIMIZED    2
-#define SW_SHOWMAXIMIZED    3
-#define SW_MAXIMIZE         3
-#define SW_SHOWNOACTIVATE   4
-#define SW_SHOW             5
-#define SW_MINIMIZE         6
-#define SW_SHOWMINNOACTIVE  7
-#define SW_SHOWNA           8
-#define SW_RESTORE          9
-#define SW_SHOWDEFAULT      10
-#define SW_FORCEMINIMIZE    11
-#define SW_MAX              11
-#define SW_NORMALNA         0xCC        /* undoc. flag in MinMaximize */
-
 typedef struct tagUSER32_UNICODE_STRING {
 	ULONG Length;
 	ULONG MaximumLength;
@@ -414,6 +174,7 @@ NTSTATUS NTAPI NtUserCallTwoParam(ULONG,ULONG,ULONG);
 HANDLE NTAPI   NtUserFindExistingCursorIcon(PUNICODE_STRING,PUNICODE_STRING,PVOID);
 HANDLE NTAPI   NtUserGetDC(HANDLE);
 LONG NTAPI     NtUserGetClassInfo(PVOID,PUNICODE_STRING,PVOID,PULONG,ULONG);
+BOOLEAN NTAPI  NtUserGetMessage(LPMSG,HWND,UINT,UINT);
 HANDLE NTAPI   NtUserGetThreadDesktop(ULONG,ULONG);
 NTSTATUS NTAPI NtUserGetThreadState(ULONG);
 NTSTATUS NTAPI NtUserInitializeClientPfnArrays(PVOID,PVOID,PVOID,PVOID);
