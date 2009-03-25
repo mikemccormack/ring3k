7614297f3d8ebc79e50b9e7cf5a5f43267233b9f
Mike McCormack <mikem@ring3k.org>
2009-03-25 08:09:10 +0000

    Remove unnecessary trace

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -127,8 +127,6 @@ NTSTATUS do_nt_syscall(ULONG id, ULONG func, ULONG *uargs, ULONG retaddr)
 	int magic = magic_val;
 	BOOLEAN win32k_func = FALSE;
 
-    dprintf("********** syscall %ld\n", func);
-
 	/* check the call number is in range */
 	if (func >= 0 && func < number_of_ntcalls)
 		ntcall = &ntcalls[func];
